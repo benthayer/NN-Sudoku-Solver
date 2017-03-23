14000d1a7e6f42fd6ea4f73d1758e6c82e7f40c1
bthayer2365 <benthayer2365@gmail.com>
2017-03-22 19:40:59 -0500

    Wrote tests, debugged puzzle generator

--- gen3.py
+++ gen3.py
@@ -7,12 +7,20 @@ all_puzzles = None
 all_solutions = None
 
 
-def str_to_array(line):
-    grid = np.zeros([9, 9], np.int32)
+def str_to_array(grid_str):
+    grid = np.zeros([9, 9], np.int8)
     for i in range(9):
-        grid[i] = list(map(lambda x: 0 if x is '.' else int(x), line[i:i+9]))
+        grid[i] = list(map(lambda x: 0 if x is '.' else int(x), grid_str[i*9:i*9 + 9]))
     grid.shape = [9**2]
     return grid
+
+
+def array_to_str(grid):
+    grid.shape = [9, 9]
+    grid_str = ''
+    for row in grid:
+        grid_str += ''.join([str(i) for i in row])
+    return grid_str
 
 
 # List of sudoku puzzles "puzzles.txt" downloaded from magictour.free.fr/sudoku.htm as "top2365.txt",
@@ -46,16 +54,36 @@ def get_permutation():
     return mapping
 
 
-def get_grid_permutations():
-    return [lambda: get_permutation() for _ in range(3)]
+def get_constrained_permutation():
+    mapping = np.zeros(9, np.int8)
+    large_mapping = np.array(range(3))
+    np.random.shuffle(large_mapping)
+    for i in range(3):
+        small_mapping = np.array(range(3))
+        np.random.shuffle(small_mapping)
+        for j in range(3):
+            mapping[i*3 + j] = large_mapping[i]*3 + small_mapping[j]
+    return mapping
+
+
+def get_grid_permutations(permutations=(None, None, None)):
+    new_permutations = [get_constrained_permutation(),
+                        get_constrained_permutation(),
+                        get_permutation()]
+
+    for i in range(3):
+        if permutations[i] is not None:
+            new_permutations[i] = permutations[i]
+
+    return tuple(new_permutations)
 
 
 def permute_rows(grid, permutation=None):
-    if not permutation:
-        permutation = get_permutation()
+    if permutation is None:
+        permutation = get_constrained_permutation()
     shape = grid.shape
     grid.shape = [9, 9]
-    new_grid = np.zeros([9, 9], np.int32)
+    new_grid = np.zeros([9, 9], np.int8)
     for i in range(9):
         new_grid[i] = grid[permutation[i]]
     grid.shape = shape
@@ -64,11 +92,11 @@ def permute_rows(grid, permutation=None):
 
 
 def permute_columns(grid, permutation=None):
-    if not permutation:
-        permutation = get_permutation()
+    if permutation is None:
+        permutation = get_constrained_permutation()
     shape = grid.shape
     grid.shape = [9, 9]
-    new_grid = np.zeros([9, 9], np.int32)
+    new_grid = np.zeros([9, 9], np.int8)
     for i in range(9):
         new_grid[:, i] = grid[:, permutation[i]]
     grid.shape = shape
@@ -77,15 +105,14 @@ def permute_columns(grid, permutation=None):
 
 
 def permute_numbers(grid, permutation=None):
-    if not permutation:
+    if permutation is None:
         permutation = get_permutation()
     shape = grid.shape
     grid.shape = [9**2]
-    new_grid = np.zeros([9**2], np.int32)
-    permutation += 1
+    new_grid = np.zeros([9**2], np.int8)
     # Zero indicates a blank spot, it should always map to itself.
     # Other numbers are also offset by 1, so this also allows for the index to match
-    permutation = np.append(np.array([0]), permutation)
+    permutation = np.append(np.array([0]), permutation + 1)
     for i in range(9**2):
         new_grid[i] = permutation[grid[i]]
     grid.shape = shape
@@ -100,6 +127,28 @@ def permute(grid, permutations=(None, None, None)):
     return grid
 
 
+def get_random_puzzle():
+    index = random.randrange(len(all_puzzles))
+    puzzle = all_puzzles[index]
+    return puzzle
+
+
+def get_permuted_puzzle():
+    puzzle = get_random_puzzle()
+    return permute(puzzle)
+
+
+def get_random_solution():
+    index = random.randrange(len(all_solutions))
+    solution = all_solutions[index]
+    return solution
+
+
+def get_permuted_solution():
+    solution = get_random_solution()
+    return permute(solution)
+
+
 def get_random_pair():
     index = random.randrange(len(all_puzzles))
     puzzle = all_puzzles[index]
@@ -108,20 +157,22 @@ def get_random_pair():
 
 
 def permute_pair(puzzle, solution, permutations=(None, None, None)):
-    for i in range(3):
-        if not permutations[i]:
-            permutations[i] = get_permutation()
+    permutations = get_grid_permutations(permutations)
     puzzle = permute(puzzle, permutations)
     solution = permute(solution, permutations)
     return puzzle, solution
+
+
+def get_permuted_pair():
+    puzzle, solution = get_random_pair()
+    return permute_pair(puzzle, solution)
 
 
 def get_batch(batch_size=1000):
     puzzle_batch = []
     solution_batch = []
     for i in range(batch_size):
-        puzzle, solution = get_random_pair()
-        puzzle, solution = permute_pair(puzzle, solution)
+        puzzle, solution = get_permuted_pair()
 
         puzzle.shape = [9**2]
         solution.shape = [9**2]
--- tests/test_gen3.py
+++ tests/test_gen3.py
@@ -1,49 +1,168 @@
-def test_lengths():
-    # Make sure the lengths of all_puzzles and all_solutions are the same and == 2365
-    pass
+import unittest
 
 
-def test_pair():
-    # Make sure the computed solution of a puzzle matches the given solution
-    pass
+import gen3
+
+from sudokulib.grid import StringGrid, InvalidGrid
+from sudokulib.solver import SudokuSolver
+
+import numpy as np
 
 
-def test_permuted_pair():
-    # Make sure a pair that has been permuted still corresponds to the correct answer
-    pass
+class TestGen3(unittest.TestCase):
+    def test_array_to_string(self):
+        grid = np.array([[5, 3, 8, 6, 4, 2, 9, 1, 7],
+                         [4, 5, 2, 8, 7, 3, 1, 6, 9],
+                         [7, 8, 3, 1, 9, 4, 6, 2, 5],
+                         [3, 6, 9, 5, 1, 8, 2, 7, 4],
+                         [1, 7, 6, 9, 2, 5, 4, 8, 3],
+                         [6, 2, 4, 7, 5, 9, 8, 3, 1],
+                         [2, 4, 7, 3, 8, 1, 5, 9, 6],
+                         [8, 9, 1, 4, 6, 7, 3, 5, 2],
+                         [9, 1, 5, 2, 3, 6, 7, 4, 8]])
+        grid_str = '538642917452873169783194625369518274176925483624759831247381596891467352915236748'
+        self.assertEqual(gen3.array_to_str(grid), grid_str)
 
+    def test_string_to_array(self):
+        grid = np.array([[5, 3, 8, 6, 4, 2, 9, 1, 7],
+                         [4, 5, 2, 8, 7, 3, 1, 6, 9],
+                         [7, 8, 3, 1, 9, 4, 6, 2, 5],
+                         [3, 6, 9, 5, 1, 8, 2, 7, 4],
+                         [1, 7, 6, 9, 2, 5, 4, 8, 3],
+                         [6, 2, 4, 7, 5, 9, 8, 3, 1],
+                         [2, 4, 7, 3, 8, 1, 5, 9, 6],
+                         [8, 9, 1, 4, 6, 7, 3, 5, 2],
+                         [9, 1, 5, 2, 3, 6, 7, 4, 8]])
+        grid.shape = [9**2]
+        grid_str = '538642917452873169783194625369518274176925483624759831247381596891467352915236748'
+        self.assertTrue((gen3.str_to_array(grid_str) == grid).all())
 
-def test_permutations_valid():
-    # Make sure permuted solutions are valid boards
-    pass
+    def test_lengths(self):
+        # Make sure the lengths of all_puzzles and all_solutions are the same and == 2365
+        self.assertEqual(len(gen3.all_puzzles), len(gen3.all_solutions))
+        self.assertNotEqual(len(gen3.all_puzzles), 0)
 
+    def solve_pair(self, puzzle, solution):
+        puzzle_str = gen3.array_to_str(puzzle)
+        solver = SudokuSolver(puzzle_str, grid_class=StringGrid)
+        solver.run()
+        computed_solution = str(solver.grid.layer)
+        solution_str = gen3.array_to_str(solution)
+        self.assertEqual(computed_solution, solution_str)
 
-def test_rows():
-    # Make sure you can find all the rows in a permutation of the original
-    pass
+    def test_pair(self):
+        # Make sure the computed solution of a puzzle matches the given solution
+        puzzle, solution = gen3.get_random_pair()
 
+        self.solve_pair(puzzle, solution)
 
-def test_columns():
-    # Make sure you can find all the columns in a permutation of the original
-    pass
+    def test_permutations_valid(self):
+        # Make sure permuted solutions are valid boards
+        for _ in range(10):
+            solution = gen3.get_permuted_solution()
+            solution = gen3.array_to_str(solution)
+            grid = StringGrid(solution)
+            try:
+                self.assertTrue(grid.validate())
+            except InvalidGrid:
+                self.fail()
 
+    def test_permuted_row_pair(self):
+        # Make sure a pair that has been permuted still corresponds to the correct answer
+        puzzle, solution = gen3.get_random_pair()
+        permutation = gen3.get_constrained_permutation()
+        puzzle = gen3.permute_rows(puzzle, permutation)
+        solution = gen3.permute_rows(solution, permutation)
 
-def test_numbers():
-    # Make sure there is the correct amount of every number
-    pass
+        self.solve_pair(puzzle, solution)
 
+    def test_permuted_column_pair(self):
+        # Make sure a pair that has been permuted still corresponds to the correct answer
+        puzzle, solution = gen3.get_random_pair()
+        permutation = gen3.get_constrained_permutation()
+        puzzle = gen3.permute_columns(puzzle, permutation)
+        solution = gen3.permute_columns(solution, permutation)
 
-def test_row_shapes():
-    # Make sure the shapes are both correct for board and new_board
-    pass
+        self.solve_pair(puzzle, solution)
 
+    def test_permuted_number_pair(self):
+        # Make sure a pair that has been permuted still corresponds to the correct answer
+        puzzle, solution = gen3.get_random_pair()
+        permutation = gen3.get_permutation()
+        puzzle = gen3.permute_numbers(puzzle, permutation)
+        solution = gen3.permute_numbers(solution, permutation)
 
-def test_column_shapes():
-    # Make sure the shapes are both correct for board and new_board
-    pass
+        self.solve_pair(puzzle, solution)
 
+    def test_permuted_pair(self):
+        # Make sure a pair that has been permuted still corresponds to the correct answer
+        puzzle, solution = gen3.get_permuted_pair()
 
-def test_number_shapes():
-    # Make sure the shapes are both correct for board and new_board
-    pass
+        self.solve_pair(puzzle, solution)
 
+    def test_rows(self):
+        # Make sure you can find all the rows in a permutation of the original
+        puzzle = gen3.get_random_puzzle()
+        puzzle.shape = [9, 9]
+        rows = set()
+        
+        for i in range(9):
+            rows.add(tuple(puzzle[i, :]))
+        permuted_puzzle = gen3.permute_rows(puzzle)
+        
+        for i in range(9):
+            row = tuple(permuted_puzzle[i, :])
+            self.assertIn(row, rows)
+            rows.difference_update({row})
+
+    def test_columns(self):
+        # Make sure you can find all the columns in a permutation of the original
+        puzzle = gen3.get_random_puzzle()
+        puzzle.shape = [9, 9]
+        cols = set()
+        
+        for i in range(9):
+            cols.add(tuple(puzzle[:, i]))
+        permuted_puzzle = gen3.permute_columns(puzzle)
+        
+        for i in range(9):
+            col = tuple(permuted_puzzle[:, i])
+            self.assertIn(col, cols)
+            cols.difference_update({col})
+
+    def test_numbers(self):
+        # Make sure all numbers are mapped correctly
+        puzzle = gen3.get_random_puzzle()
+        permutation = gen3.get_permutation()
+        permuted_puzzle = gen3.permute_numbers(puzzle, permutation)
+        puzzle.shape = permuted_puzzle.shape = [9**2]
+        for i in range(9**2):
+            num = puzzle[i]
+            if num == 0:
+                self.assertEqual(permuted_puzzle[i], 0)
+            else:
+                self.assertEqual(permuted_puzzle[i], permutation[num - 1] + 1)
+
+    def test_row_shapes(self):
+        # Make sure the shapes are both correct for board and new_board
+        board = gen3.get_random_puzzle()
+        shape = board.shape
+        new_board = gen3.permute_rows(board)
+        self.assertEqual(board.shape, shape)
+        self.assertEqual(new_board.shape, shape)
+
+    def test_column_shapes(self):
+        # Make sure the shapes are both correct for board and new_board
+        board = gen3.get_random_puzzle()
+        shape = board.shape
+        new_board = gen3.permute_columns(board)
+        self.assertEqual(board.shape, shape)
+        self.assertEqual(new_board.shape, shape)
+
+    def test_number_shapes(self):
+        # Make sure the shapes are both correct for board and new_board
+        board = gen3.get_random_puzzle()
+        shape = board.shape
+        new_board = gen3.permute_numbers(board)
+        self.assertEqual(board.shape, shape)
+        self.assertEqual(new_board.shape, shape)
